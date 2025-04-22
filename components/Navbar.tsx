1165a457a73bf5d588686b68fb1b67e1b9e207ca
RshnMng <155337224+RshnMng@users.noreply.github.com>
2025-04-22 18:00:20 -0400

    working on navbar and footer

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import CustomButton from './CustomButton'
+
+
+export const Navbar = () => {
+  return (
+    <header className='w-full absolute z-10 bg-teal-500'>
+        <div>check</div>
+    </header>
+  )
+}
